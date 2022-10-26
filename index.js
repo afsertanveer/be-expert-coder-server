14ef3c5f9ab6cf8d639133076b9caac9d2c1eb58
Abul Kawser <uiu.afser@gmail.com>
2022-10-27 00:58:20 +0600

    API for all courses created

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get("/", (req, res) => {
   res.send("Be Expert Coder API Running");
 });
-
+app.get('/courses',(req,res)=>{
+    console.log(courses);
+    res.send(courses);
+})
 
 app.listen(port, () => {
-  console.log("Dragon News Server running on port ", port);
+  console.log("Be Expert Coder running on port ", port);
 });
 
